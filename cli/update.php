226dd184e33baf64d71d7156aaef8ae8dce3b6a0
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-06-09 19:57:43 +0300

    обновление до 8.x

---
+++ cli/update.php
@@ -0,0 +1,32 @@
+<?php
+
+$config        = include __DIR__ . '/config.php';
+$path_plugin   = __DIR__ . '/../libraries';
+$path          = __DIR__ . '/../libraries/vendor/mpdf/mpdf/ttfonts';
+$manifest_path = __DIR__ . '/../jmpdf.xml';
+
+shell_exec('cd ' . $path_plugin . '; php ' . $config['composer_path'] . ' require mpdf/mpdf');
+
+
+$manifest          = file_get_contents($manifest_path);
+$composer_lock     = json_decode(file_get_contents($path_plugin . '/composer.lock'), JSON_OBJECT_AS_ARRAY);
+$mpdf_find_version = '';
+
+foreach ($composer_lock['packages'] as $package)
+{
+	if ($package['name'] === 'mpdf/mpdf')
+	{
+		$mpdf_find_version = str_replace('v', '', $package['version']);
+	}
+}
+
+$manifest = preg_replace_callback("#\<version\>.*?\<\/version\>#isu", function ($matches) use ($mpdf_find_version) {
+	return '<version>' . $mpdf_find_version . '</version>';
+}, $manifest);
+
+file_put_contents($manifest_path, $manifest);
+
+echo "update end \n";
+
+include __DIR__ . '/clearFonts.php';
+//include __DIR__ . '/build.php';
--- jmpdf.php
+++ jmpdf.php
@@ -5,70 +5,126 @@ defined('_JEXEC') or die;
 use Joomla\Registry\Registry;
 use Mpdf\Mpdf;
 
+include_once JPATH_LIBRARIES . DIRECTORY_SEPARATOR . '/mpdf/libraries/vendor/autoload.php';
+
 class JMpdf
 {
 
-    protected $config;
+	protected $config;
 
 
-    public function __construct($html = '', $uconfig = [])
-    {
+	public function __construct($html = '', $uconfig = [])
+	{
 
-        JLoader::registerNamespace('Mpdf', JPATH_LIBRARIES . DIRECTORY_SEPARATOR . 'mpdf');
-        $config = new Registry();
-        $config->loadArray(include __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+		$config = new Registry();
+		$config->loadArray(include __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
 
-        foreach ($uconfig as $key => $value) {
-            $config->set($key, $value);
-        }
+		foreach ($uconfig as $key => $value)
+		{
+			$config->set($key, $value);
+		}
 
-        $this->config = $config;
-        $this->mpdf = new Mpdf($config->toArray());
-        // If you want to change your document title,
-        // please use the <title> tag.
-        $this->mpdf->SetTitle('Document');
-        $this->mpdf->SetAuthor( $config->get('author', '') );
-        $this->mpdf->SetCreator( $config->get('creator', '') );
-        $this->mpdf->SetSubject( $config->get('subject', '') );
-        $this->mpdf->SetKeywords( $config->get('keywords', '') );
-        $this->mpdf->SetDisplayMode( $config->get('display_mode', '') );
-        if (!empty($config->get('instanceConfigurator', '')) && is_callable(($this->config->get('instanceConfigurator')))) {
-            $this->config->get('instanceConfigurator')($this->mpdf);
-        }
-        $this->mpdf->WriteHTML($html);
-    }
+		$config->set('fontdata', [
+			"dejavusans" => [
+				'R' => "DejaVuSans.ttf",
+				'B' => "DejaVuSans-Bold.ttf",
+				'I' => "DejaVuSans-Oblique.ttf",
+				'BI' => "DejaVuSans-BoldOblique.ttf",
+				'useOTL' => 0xFF,
+				'useKashida' => 75,
+			]
+		]);
+		$config->set('default_font', 'dejavusans');
 
-    protected function getConfig($key)
-    {
-        return $this->config->get($key, '');
-    }
+		$this->config = $config;
+		$this->mpdf   = new Mpdf($config->toArray());
 
-    public function setProtection($permisson, $userPassword = '', $ownerPassword = '')
-    {
-        if (func_get_args()[2] === NULL) {
-            $ownerPassword = bin2hex(openssl_random_pseudo_bytes(8));
-        };
-        return $this->mpdf->SetProtection($permisson, $userPassword, $ownerPassword);
-    }
+		// If you want to change your document title,
+		// please use the <title> tag.
+		$this->mpdf->SetTitle('Document');
+		$this->mpdf->SetAuthor($config->get('author', ''));
+		$this->mpdf->SetCreator($config->get('creator', ''));
+		$this->mpdf->SetSubject($config->get('subject', ''));
+		$this->mpdf->SetKeywords($config->get('keywords', ''));
+		$this->mpdf->SetDisplayMode($config->get('display_mode', ''));
 
-    public function output()
-    {
-        return $this->mpdf->Output('', 'S');
-    }
+		if (!empty($config->get('instanceConfigurator', '')) && is_callable(($this->config->get('instanceConfigurator'))))
+		{
+			$this->config->get('instanceConfigurator')($this->mpdf);
+		}
 
-    public function save($filename)
-    {
-        return $this->mpdf->Output($filename, 'F');
-    }
+		$this->mpdf->WriteHTML($html);
+	}
 
-    public function download($filename = 'document.pdf')
-    {
-        return $this->mpdf->Output($filename, 'D');
-    }
 
-    public function stream($filename = 'document.pdf')
-    {
-        return $this->mpdf->Output($filename, 'I');
-    }
+	/*
+	 * Magical method for Mpdf
+	 *
+	 */
+	public function __call($name, $arguments)
+	{
+		if (method_exists($this->mpdf, $name))
+		{
+			call_user_func_array([$this->mpdf, $name], $arguments);
+		}
+	}
+
+
+	protected function getConfig($key)
+	{
+		return $this->config->get($key, '');
+	}
+
+
+	public function setProtection($permisson, $userPassword = '', $ownerPassword = '')
+	{
+		if (func_get_args()[2] === null)
+		{
+			$ownerPassword = bin2hex(openssl_random_pseudo_bytes(8));
+		}
+
+		return $this->mpdf->SetProtection($permisson, $userPassword, $ownerPassword);
+	}
+
+
+	public function output()
+	{
+		return $this->mpdf->Output('', 'S');
+	}
+
+
+	public function save($filename)
+	{
+		return $this->mpdf->Output($filename, 'F');
+	}
+
+
+	public function download($filename = 'document.pdf')
+	{
+		return $this->mpdf->Output($filename, 'D');
+	}
+
+
+	public function stream($filename = 'document.pdf')
+	{
+		return $this->mpdf->Output($filename, 'I');
+	}
+
+
+	public function addFonts($font_path, $fonts)
+	{
+		$this->mpdf->AddFontDirectory($font_path);
+
+		foreach ($fonts as $font)
+		{
+			$this->mpdf->AddFont($font['family'], $font['style'] ?? '');
+		}
+	}
+
+
+	public function setFont($family, $style = '', $size = 0, $write = true, $forcewrite = false)
+	{
+		$this->mpdf->SetFont($family, $style, $size, $write, $forcewrite);
+	}
 
 }
