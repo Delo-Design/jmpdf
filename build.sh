5bb1a4bb6c341ce10ee111d62fd63bc3df540b43
dmitriitsymbal <cymbal@delo-design.ru>
2024-01-12 13:00:33 +0300

    правки скрипта сборки

--- build.sh
+++ build.sh
@@ -2,26 +2,4 @@
 
 # требования: composer, zip, unzip, curl, git, настроенный доступ ssh в гитхабе
 
-rm -rf tmpbuild
-mkdir tmpbuild
-cd tmpbuild
-
-REPO=$1
-BRANCH=$2
-FOLDER=$3
-
-if [[ "${BRANCH}" == "master" ]]
-then
-  BRANCH=""
-fi
-
-if [[ "${BRANCH}" == "main" ]]
-then
-  BRANCH=""
-fi
-
-pwd
-
-git clone ${BRANCH} git@github.com:"${REPO}".git
-
 ls -l
