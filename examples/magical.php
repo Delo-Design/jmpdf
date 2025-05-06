d3b81a178fb7ceccf4a8e529234a1f49cf6b3f42
Artem Vasilev <kern.usr@gmail.com>
2025-05-06 12:48:54 +0300

    Поправил код

--- examples/magical.php
+++ examples/magical.php
@@ -7,8 +7,8 @@
 
 $html     = '<b>Мой текст какой-то с шрифтом alaruss</b>';
 $filename = 'example.pdf';
-JLoader::register('JMpdf', JPATH_LIBRARIES . '/mpdf/jmpdf.php');
-$pdf = new JMpdf($html);
+JLoader::registerNamespace('JMpdf', JPATH_LIBRARIES . '/mpdf/jmpdf.php');
+$pdf = new \Joomla\Libraries\JMpdf\JMpdf($html);
 
 $pdf->SetAuthor('Я автор этого pdf'); // обращаемся напрямую к mpdf с помощью магического метода __call, который реализован в JMpdf
 
